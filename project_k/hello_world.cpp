5cf83ee768c8d2c235956e3cafe7385fa9032cda
Giri <guntigiridhar06@gmail.com>
2023-08-27 20:52:32 +0530

    New changes commit

--- project_k/hello_world.cpp
+++ project_k/hello_world.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main{
-    std::cout << "Hello World";
+    std::cout << "Hello World Practice";
     std::cout << "Hello World Demo";
+    std::cout << "Hello World Demo 123";
 }
